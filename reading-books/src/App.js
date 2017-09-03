4bd20a65c3fb639bd7149f2ae9b1eea4fddb208e
joe zheng <yunfei_z@163.com>
2017-09-03 20:11:51 +0800

    新增学习项目

--- reading-books/src/App.js
+++ reading-books/src/App.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit<code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
